39c30c62c1c832170d20d13e72b9d75aaf7b40a0
Ashish <ashish.ojha@xoriant.com>
2020-10-15 23:32:59 +0530

    Jest test set up

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+// JEST configuration file, see link for more information
+
+
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  preset: 'jest-preset-angular',
+  roots: ['<rootDir>/src/'],
+  testMatch: ['**/+(*.)+(spec).+(ts)'],
+  // setupFilesAfterEnv: ['<rootDir>/src/test.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/test.jest.ts'],
+  collectCoverage: true,
+  coverageReporters: ['html'],
+  coverageDirectory: 'coverage/my-app',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+    prefix: '<rootDir>/'
+  })
+};
